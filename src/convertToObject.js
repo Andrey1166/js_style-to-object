699510db8e86bda5a5d73ae124eef45a85607429
Andrey1166 <aannddrreeyy266@gmail.com>
2025-08-04 06:40:54 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,6 +12,7 @@ function convertToObject(sourceString) {
     .split(';')
     .filter((item) => item.includes(':'))
     .map((item) => item.trim().split(':', 2))
+    .filter((item) => item[0] !== '' && item[1] !== '')
     .forEach(function (item) {
       result[item[0].trim()] = item[1].trim();
     });
